df3440a28fe290974ab0f02022269ba4a584e8c9
Ben <nebstero@yahoo.com>
2023-07-12 16:35:10 -0700

    HTML labeeling was chnaged so that page will display properly

--- mod-01.js
+++ mod-01.js
@@ -8,7 +8,10 @@ const questions = [
   "Are you wearing shoes right now?", 
   "Is there a roof over your head?",
   "What is the opposite of 'no' ?",
-  "Is the Earth a cube?",];
+  "Is the Earth a cube?",
+  "Guess how many times in a week I sweep my apartment (Hint: It's a number between 1 and 10)",
+
+];
 
 // Array to store the answers
 const answers = [];
@@ -24,4 +27,27 @@ for (let i = 0; i < answers.length; i++) {
   console.log("Answer " + (i + 1) + ": " + answers[i]);
 }
 
+// Guessing game
+const correctNumber = 7;
+let attempts = 4;
+let guessedCorrectly = false;
 
+while (attempts > 0) {
+  const guess = parseInt(prompt("Guess how many times in a week I sweep my apartment? (Hint: It's a number between 1 and 10):"));
+
+  if (guess === correctNumber) {
+    alert("Correct! I like to keep my living environment tidy 🧹 ");
+    guessedCorrectly = true;
+    break;
+  } else if (guess > correctNumber) {
+    alert("Think lower! Guess again.");
+  } else {
+    alert("Oh, so you think that I'm a disgusting, filthy, uncivilized animal? Try again.");
+  }
+
+  attempts--;
+}
+
+if (!guessedCorrectly) {
+  alert("Well, you tried. The correct number is " + correctNumber + ".");
+}
